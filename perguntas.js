a53f7f807929f7ca2da00cdb796999f53f2e1e53
Alexpi100 <00001110816959sp@al.educacao.sp.gov.br>
2024-11-13 09:49:55 -0300

    FlashcardsProj3

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Html?',
+    'HTML é a linguagem usada para estruturar páginas na web, definindo elementos como textos, imagens e links. '
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Inglaterra?',
+    'A capital da Inglaterra é Londres'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma div?',
+    'A div é uma tag HTML usada para agrupar e organizar elementos na página, geralmente para aplicar estilos ou manipulação com CSS e JavaScript.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'como se diz fogo em inglês?',
+    'Fogo em inglês se diz fire (FAIRE)'
+)
